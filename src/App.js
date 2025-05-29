1bc33e2bd4435814aedea89132dabd5fed8473e3
razeen <makerrazeen2301@gmail.com>
2025-05-29 23:50:21 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Assest/CSS/style.css';
-import './Assest/CSS/media.css'
+import './Assest/CSS/media.css';
+
 import T1 from './Template/T1'; 
 import T2 from './Template/T2'; 
 import T3 from './Template/T3'; 
 import T4 from './Template/T4'; 
-import T5 from './Template/T5'; 
-
+// import T5 from './Template/T5'; 
 
 import { contactInfo } from "./Template/Config";
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
-    <div>
-    {/* <T1 {...contactInfo} /> */}
-    {/* <T2 {...contactInfo} /> */}
-    {/* <T3 {...contactInfo} /> */}
-    <T4 {...contactInfo} />
-    {/* <T5 {...contactInfo} /> */}
-
-
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<T1 {...contactInfo} />} />
+        <Route path="/t1" element={<T1 {...contactInfo} />} />
+        <Route path="/t2" element={<T2 {...contactInfo} />} />
+        <Route path="/t3" element={<T3 {...contactInfo} />} />
+        <Route path="/t4" element={<T4 {...contactInfo} />} />
+        {/* <Route path="/t5" element={<T5 {...contactInfo} />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
